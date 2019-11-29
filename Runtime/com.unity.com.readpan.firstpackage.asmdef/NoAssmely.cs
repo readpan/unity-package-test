6fee3e51d74871d3fc9bc4f52d5c90e1c10f2277
潘健博 <panjianbo@basecity.com>
2019-11-29 11:25:52 +0800

    升级版本

---
+++ Runtime/com.unity.com.readpan.firstpackage.asmdef/NoAssmely.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace System
+{
+    public class NoAssmely : UnityEngine.MonoBehaviour
+    {
+        private void Start()
+        {
+            Debug.Log("Hello World");
+        }
+    }
+}

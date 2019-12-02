4f3141a21ff8c2b981f5c6ff6ba9814823404776
潘健博 <panjianbo@basecity.com>
2019-12-02 16:26:00 +0800

    升级版本

---
+++ Tests/Runtime/TestTest.cs
@@ -0,0 +1,28 @@
+﻿
+using NUnit.Framework;
+
+public class HpCompTests
+{
+    //测试被攻击之后伤害数值是否和预期值相等
+    [Test]
+    public void TakeDamage_BeAttacked_HpEqual()
+    {
+        //Arrange
+        HpComp health = new HpComp();
+        health.currentHp = 100;
+        //Act
+        health.TakeDamage(50);
+        //Assert
+        Assert.AreEqual(50f, health.currentHp);
+    }
+
+    class HpComp
+    {
+        public int currentHp;
+
+        public void TakeDamage(int value)
+        {
+            currentHp -= value;
+        }
+    }
+}
